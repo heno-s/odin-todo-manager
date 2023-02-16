009142321f8592b87375cffcc54a094f8df3ef64
janko12 <henrich.sury97@gmail.com>
2023-02-16 21:58:28 +0100

    Make create body and set priority separate functions

--- src/displayController.js
+++ src/displayController.js
@@ -65,58 +65,9 @@ function _createTodo(todo) {
     const priorityDOM = document.createElement("input");
     priorityDOM.type = "checkbox";
     priorityDOM.classList.add("priority");
+    _setPriority(priorityDOM, priority);
 
-    switch (+priority) {
-        case 0:
-            priorityDOM.classList.add("priority-low");
-            break;
-        case 1:
-            priorityDOM.classList.add("priority-medium");
-            break;
-
-        case 2:
-            priorityDOM.classList.add("priority-high");
-            break;
-        default:
-            priorityDOM.classList.add("priority-low");
-            break;
-    }
-
-    const todoBody = document.createElement("div");
-    todoBody.classList.add("todo-body");
-
-    const todoTitle = document.createElement("div");
-    todoTitle.classList.add("todo-title");
-    todoTitle.textContent = title;
-
-    todoBody.appendChild(todoTitle);
-
-    if (description) {
-        const todoDescription = document.createElement("div");
-        todoDescription.classList.add("todo-description");
-        todoDescription.textContent =
-            description.substr(0, 50) +
-            (description.length > 50 ? "..." : "");
-
-        todoBody.appendChild(todoDescription);
-    }
-    if (dueDate) {
-        const todoDueDate = document.createElement("div");
-        todoDueDate.classList.add("todo-due-date");
-
-        const calendar = document.createElement("div");
-        calendar.classList.add("mdi");
-        calendar.classList.add("mdi-calendar-clock-outline");
-
-        const todoDate = document.createElement("div");
-        todoDate.classList.add("todo-date");
-        todoDate.textContent = dueDate;
-
-        todoDueDate.appendChild(calendar);
-        todoDueDate.appendChild(todoDate);
-
-        todoBody.appendChild(todoDueDate);
-    }
+    const todoBody = _createTodoBody(title, description, dueDate);
 
     const deleteIcon = document.createElement("div");
     deleteIcon.classList.add("mdi");
@@ -152,7 +103,7 @@ function _cloneTodoForm() {
 }
 
 function createEditForm(todo) {
-    if (document.querySelector(".todo-form").dataset.id) {
+    if (document.querySelector("#edit-form")) {
         return null;
     }
     const { title, description, priority, dueDate, id } = todo;
@@ -201,49 +152,12 @@ function editTodo(todo) {
     const { title, description, priority, id, dueDate } = todo;
 
     const todoDOM = document.getElementById(id);
-    const todoBody = todoDOM.querySelector(".todo-body");
-    const editForm = document.querySelector("#edit-form");
-
-    todoDOM.querySelector(".todo-title").textContent = title;
-    todoDOM.querySelector(".priority").value = priority;
-    let descriptionDOM = todoDOM.querySelector(".todo-description");
-    let dateDOM = todoDOM.querySelector(".todo-due-date");
-
-    if (!description) {
-        descriptionDOM && descriptionDOM.remove();
-    } else {
-        if (!descriptionDOM) {
-            descriptionDOM = document.createElement("div");
-            descriptionDOM.classList.add("todo-description");
-            todoBody.appendChild(description);
-        }
-
-        descriptionDOM.textContent =
-            description.substr(0, 50) +
-            (description.length > 50 ? "..." : "");
-    }
-
-    if (!dueDate) {
-        dateDOM && dateDOM.remove();
-    } else {
-        if (!dateDOM) {
-            dateDOM = document.createElement("div");
-            dateDOM.classList.add("todo-due-date");
-            const calendar = document.createElement("div");
-            calendar.classList.add("mdi");
-            calendar.classList.add("mdi-calendar-clock-outline");
-
-            const todoDate = document.createElement("div");
-            todoDate.classList.add("todo-date");
-            todoDate.textContent = dueDate;
-
-            dateDOM.appendChild(calendar);
-            dateDOM.appendChild(todoDate);
-        }
-        dateDOM.querySelector(".todo-date").textContent = dueDate;
-    }
+    _setPriority(todoDOM.querySelector(".priority"), priority);
+    const todoBody = _createTodoBody(title, description, dueDate);
+    todoDOM.querySelector(".todo-body").replaceWith(todoBody);
 
     todoDOM.classList.remove("hide");
+    const editForm = document.querySelector("#edit-form");
     editForm && editForm.remove();
     return true;
 }
@@ -251,6 +165,61 @@ function editTodo(todo) {
 function removeTodo(id) {
     const todo = document.getElementById(id);
     todo.remove();
+}
+
+function _createTodoBody(title, description, dueDate) {
+    const todoBody = document.createElement("div");
+    todoBody.classList.add("todo-body");
+
+    const todoTitle = document.createElement("div");
+    todoTitle.classList.add("todo-title");
+    todoTitle.textContent = title;
+
+    todoBody.appendChild(todoTitle);
+
+    if (description) {
+        const todoDescription = document.createElement("div");
+        todoDescription.classList.add("todo-description");
+        todoDescription.textContent =
+            description.substr(0, 50) +
+            (description.length > 50 ? "..." : "");
+
+        todoBody.appendChild(todoDescription);
+    }
+    if (dueDate) {
+        const todoDueDate = document.createElement("div");
+        todoDueDate.classList.add("todo-due-date");
+
+        const calendar = document.createElement("div");
+        calendar.classList.add("mdi");
+        calendar.classList.add("mdi-calendar-clock-outline");
+
+        const todoDate = document.createElement("div");
+        todoDate.classList.add("todo-date");
+        todoDate.textContent = dueDate;
+
+        todoDueDate.appendChild(calendar);
+        todoDueDate.appendChild(todoDate);
+
+        todoBody.appendChild(todoDueDate);
+    }
+}
+
+function _setPriority(priorityDOM, priority) {
+    switch (+priority) {
+        case 0:
+            priorityDOM.classList.add("priority-low");
+            break;
+        case 1:
+            priorityDOM.classList.add("priority-medium");
+            break;
+        case 2:
+            priorityDOM.classList.add("priority-high");
+            break;
+        default:
+            priorityDOM.classList.add("priority-low");
+            break;
+    }
 }
 
 export default {
